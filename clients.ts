fe70ec812699b61cde75b10046734ed911652502
Benito III Guevarra <96364719+benetguevarra@users.noreply.github.com>
2025-09-04 13:58:30 -0400

    Update clients.ts

--- clients.ts
+++ clients.ts
@@ -2,14 +2,29 @@ import { Router } from "express";
 
 const router = Router();
 
-// Example route
+// Get all clients
 router.get("/", (req, res) => {
-  res.json({ message: "Clients list" });
+  res.json({ message: "List of all clients" });
 });
 
+// Get one client by ID
+router.get("/:id", (req, res) => {
+  res.json({ message: `Client with ID ${req.params.id}` });
+});
+
+// Create new client
 router.post("/", (req, res) => {
-  const newClient = req.body;
-  res.json({ message: "Client created", client: newClient });
+  res.json({ message: "Client created", client: req.body });
+});
+
+// Update client
+router.put("/:id", (req, res) => {
+  res.json({ message: `Client ${req.params.id} updated`, data: req.body });
+});
+
+// Delete client
+router.delete("/:id", (req, res) => {
+  res.json({ message: `Client ${req.params.id} deleted` });
 });
 
 export default router;
